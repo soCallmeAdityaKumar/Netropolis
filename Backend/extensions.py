dffa3024c3fac9817154df54d6302fa93c4bd5b5
Aditya kumar <adi54321kumar@gmail.com>
2024-03-01 11:58:47 +0530

    sql revert

--- Backend/extensions.py
+++ Backend/extensions.py
@@ -5,11 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 
 
-db= SQLAlchemy()
 
 connection = psycopg2.connect(
         host=os.environ['HOST_NAME'],
         database=os.environ['DATABASE'],
         user=os.environ['DB_USERNAME'],
         password=os.environ['DB_PASSWORD'])
+db= SQLAlchemy()
+
 jwt=JWTManager()
