588d1b1e6f8ecf7593663dc5e5e71e40a8c40c6e
Aditya kumar <adi54321kumar@gmail.com>
2024-03-01 11:55:40 +0530

    SQL 2

--- Backend/main.py
+++ Backend/main.py
@@ -2,12 +2,15 @@ from flask import Flask
 from extensions import db,jwt
 from auth import authUser_bp,authComp_bp
 from job import job_bp
+import os
 from flask_cors import CORS
 from models import init_db
 def create_app():
 
     app=Flask(__name__)
     app.config.from_prefixed_env()
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.environ['DB_USERNAME']}:{os.environ['DB_PASSWORD']}@{os.environ['HOST_NAME']}/{os.environ['DATABASE']}"
+
     CORS(app)
     #initialize app
     db.init_app(app)
